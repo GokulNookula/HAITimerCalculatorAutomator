a60473990a3c6a4a00264d890cc0dd6ba84de37d
GokulNookula <gokulknookula@gmail.com>
2026-05-27 22:51:22 -0700

    was able to input email for the google

--- main.py
+++ main.py
@@ -20,6 +20,23 @@ driver.get("https://ai.joinhandshake.com/fellow/15727563-a6cd-46b6-9c57-9bc1359b
 googleButton = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Continue with Google')]")))
 googleButton.click()
 
+# Wait for popup window
+WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)
+
+# Create temporary popup driver reference
+googleDriver = driver.switch_to.window(driver.window_handles[-1])
+
+# Switch actually returns None, so do this instead:
+driver.switch_to.window(driver.window_handles[-1])
+googleDriver = driver
+
+# Inputting the email
+emailInput = WebDriverWait(googleDriver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]')))
+emailInput.send_keys(secret.email)
+
+# Clicking next button
+nextButton = WebDriverWait(googleDriver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="identifierNext"]/div/button/span')))
+nextButton.click()
 
 # Keep browser open for 30 seconds
 time.sleep(400000)
